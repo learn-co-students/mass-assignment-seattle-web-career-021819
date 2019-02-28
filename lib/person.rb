931fbcc7adeb4733103c3805a3f140d0d0dccefa
isalevine <isaacdlevine@gmail.com>
2019-02-28 14:56:09 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  @@all = []
+
+  def initialize(person_hash)
+    person_hash.each do |key_symbol, value_string|
+      self.send("#{key_symbol}=", value_string)
+    end
+    @@all << self
+  end
 end
